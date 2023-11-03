544c0f5dd99b4886029136d6131bd870bba5aef4
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-11-03 11:51:55 +0000

    db linked classes created and tested for id

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,10 @@
-// create your User model here
+const { db } = require("../db/config");
+const { DataTypes } = require("sequelize");
+//
+const User = db.define("user", {
+  username: DataTypes.STRING,
+});
+
+console.log(User);
+
+module.exports = User;
--- server/src/models/User.test.js
+++ server/src/models/User.test.js
@@ -1,26 +1,25 @@
-const { describe, it, expect, beforeAll, afterAll } = require('@jest/globals')
-const { User } = require('.')
-const db = require('../db/config')
+const { describe, it, expect, beforeAll, afterAll } = require("@jest/globals");
+const User = require("./User");
+const { db } = require("../db/config");
 
-// define in global scope
-let user
+// define in global scopel
+let user;
 
 // clear db and create new user before tests
 beforeAll(async () => {
-  await db.sync({ force: true })
-  user = await User.create({ username: 'gandalf' })
-})
+  await db.sync({ force: true });
+  user = await User.create({ username: "gandalf" });
+});
 
 // clear db after tests
-afterAll(async () => await db.sync({ force: true }))
-
-describe('User', () => {
-  it('has an id', async () => {
-    expect(user).toHaveProperty('id')
-  })
+afterAll(async () => await db.sync({ force: true }));
+describe("User", () => {
+  it("has an id", async () => {
+    expect(user).toHaveProperty("id");
+  });
 
   /**
    * Create more tests
    * E.g. check that the username of the created user is actually gandalf
    */
-})
+});
